cb9905d2740949a9a2f707f470f31a3d777b5718
ankasamanyan <anikakasamanyan@gmail.com>
2023-12-21 00:12:45 +0100

    :

---
+++ frontend/src/app/responsive.service.ts
@@ -0,0 +1,14 @@
+// import { Injectable } from '@angular/core';
+// import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class ResponsiveService {
+//   // isSmallScreen$ = this.breakpointObserver.observe(Breakpoints.Small).pipe(
+//   //   map((result) => result.matches),
+//   //   shareReplay()
+//   // );
+
+//   constructor(private breakpointObserver: BreakpointObserver) {}
+// }
